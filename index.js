cc78b85458b52aeb5a9b4c3a339867fb4ce73211
ValentinPavlov <valentinpavlov38@gmail.com>
2022-12-15 23:32:12 +0300

    Complete random number game

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ const bot = new TelegramApi(token, {
 
 const chats = {}
 
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: '1', callback_data: '1'},{text: '2', callback_data: '2'},{text: '3', callback_data: '3'}],
+            [{text: '4', callback_data: '4'},{text: '5', callback_data: '5'},{text: '6', callback_data: '6'}],
+            [{text: '7', callback_data: '7'},{text: '8', callback_data: '8'},{text: '9', callback_data: '9'}],
+            [{text: '0', callback_data: '0'}]
+        ]
+    })
+}
+
+const againGameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Играть еще раз', callback_data: '/again'}]
+        ]
+    })
+}
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, 'Я загадаю тебе число от 0 до 9. Попробуй отгадать!')
+    const randomNumber = Math.floor(Math.random() *10)
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Отгадывай!', gameOptions)
+}
+
+
 const bot_start = () => {
     bot.setMyCommands([{
             command: '/start',
@@ -18,7 +45,7 @@ const bot_start = () => {
             description: 'Получить информацию'
         },
         {
-            command: '/ugadaika',
+            command: '/game',
             description: 'Игра в угадай число'
         }
     ])
@@ -32,28 +59,35 @@ const bot_start = () => {
             return bot.sendMessage(chatId, `${msg.from.first_name}, добро пожаловать на проект Fourtune24!`)
         }
 
+
         if (text === '/info') {
             return  bot.sendMessage(chatId, `${msg.chat.username}, добро пожаловать на проект Fourtune24!`)
         }
 
-        if (text === '/ugadaika') {
-            await bot.sendMessage(chatId, 'Я загадаю тебе число от 0 до 9. Попробуй отгадать!')
-            const randomNumber = Math.floor(Math.random() *10)
-            chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Отгадывай!')
+        if (text === '/game') {
+            return startGame(chatId)
         }
 
-        if (text === '/restart') {
-            await bot.sendMessage(chatId, 'Я загадаю тебе число от 0 до 9. Попробуй отгадать!')
-            const randomNumber = Math.floor(Math.random() *10)
-            chats[chatId] = randomNumber;
-            return bot.sendMessage(chatId, 'Отгадывай!')
-        }
 
         return bot.sendMessage(chatId,'Я тебя не понимаю :( Попробуй еще раз!')
 
-        
-    
+
+    })
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+        if (data == chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю! Ты отгадал цифру ${chats[chatId]}`,againGameOptions)
+        }
+        else {
+            console.log(data,chats[chatId])
+            return bot.sendMessage(chatId, `К сожалению, ты не отгадал, бот загадал цифру ${chats[chatId]}`,againGameOptions)
+        }
+        bot.sendMessage(chatId, `Ты выбрал кнопку ${data} `)
     })
 }
 
