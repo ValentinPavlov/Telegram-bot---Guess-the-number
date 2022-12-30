ed6684047ba8803dbc209e9f717bec9fd65bf969
ValentinPavlov <valentinpavlov38@gmail.com>
2022-12-30 16:41:39 +0300

    token update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramApi = require('node-telegram-bot-api')
 const { gameOptions, againGameOptions} = require("./options")
-const token = '5809078192:AAFNVDkHVSSnT4LXvDo6FRx_gpc34-mtT6A'
+
 
 const bot = new TelegramApi(token, {
     polling: true
